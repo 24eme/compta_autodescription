608f4f3d4101b037a7885b960adab9475323b618
Tangui Morlier <tangui@tangui.eu.org>
2024-10-01 19:31:10 +0200

    Permet de voir toutes les lignes des relevés y compris celles déjà associées

--- pdf/views.py
+++ pdf/views.py
@@ -70,7 +70,10 @@ def piece_associate_banque(request, md5):
     piece = Piece.objects.filter(md5=md5).first()
     file = piece.getFile()
     banques = {}
-    for banque in Banque.objects.filter(piece=None):
+    banque_objects = Banque.objects.filter(piece=None)
+    if request.GET.get('all'):
+        banque_objects = Banque.objects.all()
+    for banque in banque_objects:
         distance = 0
         nb = 0
         if piece.facture_author != "24eme" and piece.facture_author != "24ème":
@@ -84,21 +87,29 @@ def piece_associate_banque(request, md5):
         nb += 1
         distance += compare_strings(piece.filename, banque.raw)
         nb += 1
-        thediff = (int(banque.date.strftime('%s')) - int(piece.facture_date.strftime('%s'))) / (60*60*24*30)
+        thediff = 0
+        if piece.facture_date:
+            thediff = (int(banque.date.strftime('%s')) - int(piece.facture_date.strftime('%s'))) / (60*60*24*30)
         if piece.facture_date and thediff <= 1:
-            distance += thediff
+            distance += abs(thediff)
             nb += 1
         if piece.facture_prix_ttc and banque.amount:
             if piece.facture_prix_ttc == banque.amount:
                 distance += 0
                 nb += 4
             else:
-                distance += (abs(piece.facture_prix_ttc - abs(banque.amount)) / piece.facture_prix_ttc ) * 2
-                nb += 2
-        if thediff >=0:
+                diffprix = abs(abs(piece.facture_prix_ttc) - abs(banque.amount))
+                if diffprix < 10:
+                    distance += abs(diffprix) / 10
+                    nb += 1
+                else:
+                    distance += 2
+                    nb += 2
+        if request.GET.get('all') or thediff >= -1 :
             banques[banque.id] = {"distance": distance/nb, "banque": banque}
 
     banques = dict(sorted(banques.items(), key=lambda x: x[1]['distance']))
+
     return render(request, "piece_associate_banque.html", {"banques": banques, "piece": piece, "file": file, 'file_date': file.getDate()})
 
 def banque_associate_file(request, banque_id):
